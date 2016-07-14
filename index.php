38fe9bd800bc03b8fd60e7adefccc02d48981055
David Mabaso <david@sidl.co.za>
2016-07-14 16:58:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,19 @@
-<?php 
-//place this before any script you want to calculate time
+<?php
 $time_start = microtime(TRUE); 
 
-	//Configuration file:
-	
-define('project_name', 'en_kosi_v3_alph');
+require_once('/class/config.php');
 
-require($_SERVER['DOCUMENT_ROOT'].project_name.DIRECTORY_SEPARATOR.'library'.DIRECTORY_SEPARATOR.'core'.DIRECTORY_SEPARATOR.'core.php');
+require_once('/class/darkmeta.php');
 
-				
-$en_kosi_core = new en_kosi_core();
+//initialise the class
+$DarkMeta = new DarkMeta();
 
-for($x=1; $x<=$en_kosi_core->prod_instance_count; $x++)
-{
-	
-	$en_kosi_core->URL_GET($en_kosi_core->prod_instance_get[$x]);
-	
-}
-				
+//sterilz the url
+$DarkMeta->url_heartbeat('http://www.vodacom.co.za:80');
+
+$DarkMeta->URL_GET($DarkMeta->url_host);
+
 $time_end = microtime(TRUE);
-
 //dividing with 60 will give the execution time in minutes other wise seconds
 $execution_time = ($time_end - $time_start);
-
-//execution time of the script
-echo '<br> <b>Total Execution Time:</b> '.$execution_time.' Sec';
 ?>
